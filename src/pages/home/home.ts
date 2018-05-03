b264b1148f643a83cc681a8db03cedeba0dbd335
Michael Bolens <bolensmichael@gmail.com>
2018-05-03 12:07:46 -0600

    Fix file structure and add map points

---
+++ src/pages/home/home.ts
@@ -0,0 +1,98 @@
+import { Component, AfterViewInit, ViewChild, NgZone, ElementRef } from '@angular/core';
+import { NavController, Platform, ViewController } from 'ionic-angular';
+import { LocationPage } from './location/location';
+import leaflet from 'leaflet';
+import axios from 'axios';
+
+
+// LocationPage: LocationPage;
+let corner1 = leaflet.latLng(23.500, -125.230),
+corner2 = leaflet.latLng(49.923,-67.661),
+bounds = leaflet.latLngBounds(corner1, corner2);
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
+  locations: any;
+  selectedLocation: any;
+  isLocationSelected: boolean = false;
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  onInput(event){
+  // TODO: Add axios get call for searched data
+ }
+
+  ionViewDidLoad() {
+    this.getLocations();
+  }
+
+  loadmap() {
+    let markerGroup = leaflet.featureGroup();
+    this.map = leaflet.map("map")
+      .on('load', (e) => {
+        console.log('Map loaded, populating locations...');
+        console.log('Sample location', this.locations[0]);
+        this.locations.forEach((location, index) => {
+          let marker: any = leaflet.marker([location.coordinates.lat, location.coordinates.lon])
+            .bindPopup(
+              `${location.name}`
+            )
+            .openPopup()
+            .on('popupopen', (popup) => {
+              this.selectedLocation = location;
+              this.isLocationSelected = true;
+            })
+            .on('popupclose', (popup) => {
+              this.selectedLocation = {};
+              this.isLocationSelected = false;
+            });
+
+          markerGroup.addLayer(marker);
+        });
+      })
+      .addLayer(markerGroup)
+      .fitBounds(bounds);
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(this.map);
+    this.map
+      .on('locationfound', (e) => {
+
+      })
+      .on('locationerror', (err) => {
+        console.log(err.message);
+        console.log(err.stack);
+      });
+  }
+  getLocations() {
+    axios.get("http://localhost:3000/locations")
+    .then((res) => {
+      this.locations = res.data;
+      this.loadmap();
+    })
+    .catch((err) => {
+      console.log(err.message);
+      console.log(err.stack);
+    });
+  }
+  goToLocationPage() {
+    axios.get("http://localhost:3000/locations/" + this.selectedLocation._id)
+    .then(res => {
+      console.log(res);
+      this.navCtrl.push(LocationPage, res.data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      console.log(err.stack);
+    });
+  }
+
+}
---
+++ src/pages/home/location/location.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CheckInPage } from './check-in/check-in';
+import moment  from 'moment';
+
+@IonicPage()
+@Component({
+  selector: 'page-location',
+  templateUrl: 'location.html',
+})
+export class LocationPage {
+
+locationPhoto:string;
+name:string;
+nextEvent:any;
+facts:any;
+recommendations:any;
+opponentName:string;
+ticketLink:string;
+date:string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.locationPhoto = navParams.get('locationPhoto');
+    this.name = navParams.get('name');
+    this.nextEvent = navParams.get('nextEvent');
+    this.facts = navParams.get('facts');
+    this.recommendations = navParams.get('recommendations');
+    this.opponentName = navParams.get('nextEvent.opponentName');
+    this.ticketLink = navParams.get('nextEvent.ticketLink');
+    this.date = moment(this.nextEvent.date).format('MMM Do @ h:mmA');
+  }
+
+  ionViewDidLoad() {
+    console.log(this.navParams);
+  }
+
+  goToCheckInPage() {
+    this.navCtrl.push(CheckInPage);
+  }
+
+}
