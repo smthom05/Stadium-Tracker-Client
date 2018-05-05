959e78d3ddb442108e80c48939a2c1e99b04ab5b
Michael Bolens <bolensmichael@gmail.com>
2018-05-05 13:37:36 -0600

    Wire up leaderboard

--- src/pages/social/modals/leaderboard/leaderboard.ts
+++ src/pages/social/modals/leaderboard/leaderboard.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams, ViewController } from 'ionic-angular';
+import axios from 'axios';
 
 @IonicPage()
 @Component({
@@ -7,6 +8,8 @@ import { IonicPage, NavParams, ViewController } from 'ionic-angular';
   templateUrl: 'leaderboard.html',
 })
 export class LeaderboardPage {
+  ranks: any;
+  league: string;
   constructor(
     public navParams: NavParams,
     public viewCtrl: ViewController
@@ -15,7 +18,20 @@ export class LeaderboardPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LeaderboardPage');
+    this.league = 'All';
+    this.getLeaderboardStats();
+  }
+
+  getLeaderboardStats() {
+    axios.get('http://localhost:3000/leaderboard/baseball')
+      .then((res) => {
+        this.ranks = res.data;
+        console.log('Rankings', this.ranks);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        console.log(err.stack);
+      });
   }
 
   dismiss() {
